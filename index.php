d0ac20afd1b7d7ccbad7bdccaf14b9face0c5c36
MAKARYAN Raphael <contact@raphaelmakaryan.fr>
2024-10-25 01:32:39 +0200

    Adding the logo as a favicon

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BlendNations</title>
     <link rel="stylesheet" href="./public/CSS/style.css">
+    <link rel="shortcut icon" href="./public/IMG/logoW.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
